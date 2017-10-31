e4456af026787cbcc4208552523eba81faa62861
vagrant <vagrant@homestead>
2017-10-31 09:33:59 +0000

    Initial commit with obligatory beer picture

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+// making sure we don't fuck up the image contents
+// with some fucken warning messages
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// set tha variaablzzz
+$logfile = 'log.txt';
+$path = dirname(__FILE__).'/images/';
+$image = (!empty($_GET['file']) ? $_GET['file'] : 'default.png');
+
+// save ip address & user agent
+$log = file_get_contents($logfile);
+$log .= date('d.m.Y H:i:s').' - '.$_SERVER['REMOTE_ADDR']. ' - '.$_SERVER['HTTP_USER_AGENT'].' @ '.$image."\n";
+file_put_contents($logfile, $log);
+
+// rendeeeer thaaa imaaaageee
+use Imagecow\Image;
+
+if (is_file($path.$image))
+    Image::fromFile($path.$image)->show();
+else
+    Image::fromString(file_get_contents('https://i.imgur.com/T19XMgm.jpg'))->show();
