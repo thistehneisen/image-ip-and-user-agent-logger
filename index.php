050e60fa4932986d1b24a0510cb58db1b5b7cdb1
SEQ.LV <nils@influent.solutions>
2023-01-08 18:42:17 +0200

    Refactoring

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
-error_reporting(0);
-ini_set('display_errors', 0);
-
 $logfile = 'log.txt';
-$path = dirname(__FILE__).'/images/';
-$image = (!empty($_GET['file']) ? $_GET['file'] : 'default.png');
+$path = __DIR__.'/images/';
+$image = $_GET['file'] ?? 'default.png';
 
-$log = file_get_contents($logfile);
-$log .= date('d.m.Y H:i:s').' - '.$_SERVER['REMOTE_ADDR']. ' - '.$_SERVER['HTTP_USER_AGENT'].' @ '.$image."\n";
-file_put_contents($logfile, $log);
+file_put_contents($logfile, file_get_contents($logfile).date('d.m.Y H:i:s').' - '.$_SERVER['REMOTE_ADDR']. ' - '.$_SERVER['HTTP_USER_AGENT'].' @ '.$image."\n");
 
 use Imagecow\Image;
 
-if (is_file($path.$image))
-    Image::fromFile($path.$image)->show();
-else
-    Image::fromString(file_get_contents('https://i.imgur.com/T19XMgm.jpg'))->show();
+Image::fromFile(is_file($path.$image) ? $path.$image : 'https://i.imgur.com/T19XMgm.jpg')->show();
